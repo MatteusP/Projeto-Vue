26331fca012bc2ea44b0e4f0553bcbfb3ecbf4b3
Mateus Paiva <matteusp@alu.ufc.br>
2022-05-11 09:12:37 -0300

    adicionando footer e nav bar, remoção arquivos

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="nav">
+        <router-link to="/">
+            <img src="" id="Logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/Pedidos">Pedidos</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
