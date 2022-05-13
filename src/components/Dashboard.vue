c78923141204e62b3a5067485f4c510375adf368
Mateus Paiva <matteusp@alu.ufc.br>
2022-05-13 10:57:23 -0300

    mostrar pedidos salvos na tabela

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,22 +4,21 @@
       <div id="burger-table-heading">
         <div class="order-id">#:</div>
         <div>Cliente:</div>
-        <div>Pão</div>
+        <div>Pão:</div>
         <div>Carne:</div>
         <div>Opcionais:</div>
         <div>Ações:</div>
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pâo de trigo</div>
-        <div>maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
-          <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+          <ul v-for="(opcional, index) in burger.opcionais" :key="index">
+            <li>{{ opcional }}</li>
           </ul>
         </div>
         <div>
@@ -36,8 +35,25 @@
 <script>
 export default {
   name: "Dashboard",
-
-}
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    };
+  },
+  methods: {
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+      const data = await req.json();
+      this.burgers = data;
+      console.log(this.burgers);
+    },
+  },
+  mounted() {
+    this.getPedidos();
+  },
+};
 </script>
 
 <style scoped>
